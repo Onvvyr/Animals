5c248691665896e0957f4ff507360eec6e6b312f
Onvyr <nikodem.pilachowski@gmail.com>
2021-10-14 10:13:12 +0200

    Add project files.

---
+++ Animals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Animals
+{
+    class Program
+    {
+        interface IZwierze
+        {
+            void SampleMethod();
+        }
+
+        class ImplementationClass : IZwierze
+        {
+            void IZwierze.SampleMethod()
+            {
+            }
+
+            static void Main()
+            {
+                IZwierze obj = new ImplementationClass();
+
+                obj.SampleMethod();
+            }
+        }
+    }
+}
